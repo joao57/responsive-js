67bc02abc3cab79cb929cda9e119a64d63a4a4ae
joao57 <100475803+joao57@users.noreply.github.com>
2022-09-19 07:44:06 -0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack'),
+  path = require('path');
+
+var entry = __dirname + '/tmp/JS.Responsive.entryDefault.js';
+
+var config = {
+  entry: {
+    "JS.Responsive": entry,
+    "JS.Responsive.min": entry,
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    library: 'JS.Responsive',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules|bower_components/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ]
+};
+
+module.exports = config;
