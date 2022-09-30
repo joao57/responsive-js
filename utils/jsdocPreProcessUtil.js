6fa5a133e72833508f29c0c44ad941648d1f28b7
joao57 <100475803+joao57@users.noreply.github.com>
2022-09-30 08:08:03 -0300

    Create jsdocPreProcessUtil.js

---
+++ utils/jsdocPreProcessUtil.js
@@ -0,0 +1,18 @@
+
+var fs = require('fs');
+// var cheerio = require('cheerio');
+
+
+fs.readFile(__dirname + '/../JS.Responsive/jsdoc.json', 'utf-8', function(err, contents) {
+    if (err) console.error(err);
+    if(contents.search('"template": "./node_modules/ink-docstrap/template",') == -1)
+        return;
+    contents = contents.replace('"template": "./node_modules/ink-docstrap/template",', '"template": "' + __dirname + '/../JS.Responsive.jsdocTmpl/template",');
+
+    fs.writeFile(__dirname + '/../JS.Responsive/jsdoc.json', contents, function(err){
+        "use strict";
+        if(err) console.error(err);
+
+        console.log('file fix done: ', './JS.Responsive/jsdoc.json');
+    });
+});
